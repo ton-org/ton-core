6426af481661f6a7204bb48ae37ae788e57ef71b
Valerii Smirnov <valerik007.vs@gmail.com>
2025-12-03 21:16:36 +0100

    build: improved jest performance (#106)
    * improved jest performance
    
    * added singlethreaded flag
    
    * added flag example

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/","/dist/"]
+const SINGLETHREADED =
+  process.env.SINGLETHREADED === "1" || process.env.SINGLETHREADED === "true";
+/** @type {import('jest').Config} */
+const options = {
+  transform: {
+    "^.+\\.ts": "@swc/jest",
+  },
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
+
+if (SINGLETHREADED) {
+  /** setting value to undfined throws */
+  options.maxWorkers = 1;
+}
+
+module.exports = options;
