ff5dcb6ec6a2997767682dcda4bdb9a6cb4c776a
XantreDev <valerik007.vs@gmail.com>
2025-12-03 19:20:04 +0100

    added singlethreaded flag

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+const SINGLETHREADED =
+  process.env.SINGLETHREADED === "1" || process.env.SINGLETHREADED === "true";
+/** @type {import('jest').Config} */
+const options = {
   transform: {
     "^.+\\.ts": "@swc/jest",
   },
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
+
+if (SINGLETHREADED) {
+  /** setting value to undfined throws */
+  options.maxWorkers = 1;
+}
+
+module.exports = options;
