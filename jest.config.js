52d6b0d94cd9e34a9a6eab9b70d17d88aa5408bb
XantreDev <valerik007.vs@gmail.com>
2025-12-03 18:16:45 +0100

    improved jest performance

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/","/dist/"]
+  transform: {
+    "^.+\\.ts": "@swc/jest",
+  },
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
